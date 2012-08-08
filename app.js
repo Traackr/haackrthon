0ac568f4066886636efcddd0da6335b9839c9c23
Bob Corsaro <bob@embed.ly>
2012-08-08 07:04:21 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,44 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , middleware = require('./middleware')
+  , http = require('http')
+  , path = require('path')
+  , Endpoint = require('express-endpoint')
+  , connectStatsd = require('connect-statsd');
+
+var app = express()
+  , api = endpoints.Api();
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(connectStatsd({prefix: 'haackrthon.'+hostname}));
+  app.use(middleware.accessLogger());
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(Endpoint.static());
+  app.use(express.static(path.join(__dirname, 'public')));
+  app.use(Endpoint.errorHandler());
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/', routes.index);
+routes.endpoints.forEach(function(endpoint) {
+  endpoint.mount(app, ['get', 'post', 'delete', 'put']);
+});
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
