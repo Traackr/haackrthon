3505ae2bd89aa31c310e1eb3577091a525348142
Bryan Warner <bryan.warner@gmail.com>
2012-08-08 15:06:54 -0400

    Updating unit test

--- test/indexer.js
+++ test/indexer.js
@@ -11,7 +11,8 @@ describe('indexer', function() {
         id: "someid",
         name: "Bob Corsaro",
         interests: ["node.js", "linux"],
-        groups: ["Boston Javascript", "Boston Python"]
+        thumbnail_url: "http://photos1.meetupstatic.com/photos/member/a/7/e/0/member_10782976.jpeg",
+        groups: [{"group_id": "909090", "group_name": "Boston Javascript"}, {"group_id": "717171", "group_name": "Boston Python"}]
       }, function(_, data) {
         indexer.search("node.js", function(err, data) {
           data.hits.total.should.equal(1);
