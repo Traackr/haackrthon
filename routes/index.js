1c1ec34ca81b4a9692406c527d9b8f876a9c6217
Bob Corsaro <bob@embed.ly>
2012-08-08 17:20:43 -0400

    Adds batch job endpoint

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,9 @@ var Endpoint = require('express-endpoint')
   , customRules = require('../rules')
   , Indexer = require('../lib/indexer')
   , bb = require('batbelt')
-  , indexer = new Indexer(null, bb.NOOP);
+  , indexer = new Indexer(null, bb.NOOP)
+  , BatchJobs = require('../lib/batchjobs')
+  , batchJobs = new BatchJobs();
 
 var search = new Endpoint({
   path: '/1/search',
@@ -26,8 +28,35 @@ var search = new Endpoint({
  }
 });
 
+var meetupCrawl = new Endpoint({
+  path: '/1/meetup/crawl',
+  description: 'Crawn meetup.com with a seed meetup group URL',
+  example: '/1/meetup/crawl?url=http://meetup.bostonpython.com/&apiKey=shhhh',
+  parameters: [
+    {
+      name: 'apiKey',
+      rules: ['required', 'apiKey', 'once'],
+      description: 'Admin api key'
+    },
+    {
+      name: 'url',
+      rules: ['required', 'url', 'once'],
+      description: 'Meetup to seed for crawl.'
+    }
+ ],
+ handler: function(req, res) {
+   batchJobs.crawlMeetup(req.endpointParmas.url.href, function(err, response) {
+     if (err) {
+       throw err;
+     } else {
+       res.renderEndpointData(response);
+     }
+   });
+ }
+});
+
 exports.search = search
-exports.endpoints = [search]
+exports.endpoints = [search, meetupCrawl]
 
 exports.index = function(req, res) {
   res.render('index', { title: 'Heystack' })
